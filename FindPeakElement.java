380075bca97d7ccf163dacf4b766853afb0f6012
iharshit766 <harshi766766@gmail.com>
2025-03-26 10:01:21 +0530

    [Harshit] ADD: program to  Find the Peak Element in an Array

---
+++ FindPeakElement.java
@@ -0,0 +1,25 @@
+public class FindPeakElement {
+    public static int findPeakElement(int[] arr) {
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                right = mid; // Peak is in the left half
+            } else {
+                left = mid + 1; // Peak is in the right half
+            }
+        }
+
+        return left; // Index of a peak element
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 3, 5, 4, 2};
+        int peakIndex = findPeakElement(array);
+
+        System.out.println("Peak element index: " + peakIndex);
+        System.out.println("Peak element: " + array[peakIndex]);
+    }
+}
